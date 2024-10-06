a17fd71de02cb9e7392e6a24d93c9c8ada551412
AreebKhanAnwar <areebkhanp1@gmail.com>
2024-10-06 11:37:28 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Questuion No.1 or No.2
+// var user = +prompt("Type any integer number")
+// console.log(user)
+// var user_ro = Math.round(user)
+// console.log(user_ro);
+// var user_flor = Math.floor(user)
+// console.log(user_flor);
+// var user_ceil = Math.ceil(user)
+// console.log(user_ceil);
+
+// Question No.3
+
+// var abso = +prompt("Write any number")
+// console.log( Math.abs(abso))
+
+
+
+// Question No.4
+
+// var user = prompt("Enter any no. from dice")
+// var dice = Math.floor(Math.random()*7)
+
+// if (user == dice) {
+//     console.log(dice + " You Win")
+    
+// } else {
+//     console.log(dice + " Computer Win")
+// }
